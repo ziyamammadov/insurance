4b61a149a90ad32dd59cbcec6129aec3a55b2d8e
Ziya <mmmdovziya900@gmail.com>
2020-02-11 22:57:56 -0800

    initial commit

---
+++ src/test/java/com/azericard/insurance/InsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azericard.insurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
